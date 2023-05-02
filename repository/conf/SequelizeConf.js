5764bfb83436d13ec99b8a36393d2ca29e2297d7
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2023-05-02 16:21:50 +0300

    chore: database access conf

--- repository/conf/SequelizeConf.js
+++ repository/conf/SequelizeConf.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize")
-
+require("dotenv").config();
 
 const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME, process.env.DB_PASSWORD , {
     host: process.env.DB_HOST,
@@ -11,5 +11,11 @@ const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME, pro
         idle: 10000
     }
 });
+
+sequelize.authenticate().then(()=>{
+    console.log("success");
+}).catch((err)=>{
+    console.log("failure"+ err);
+})
 
 module.exports = {sequelize}
